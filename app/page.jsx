31d2cea8dcb02d00a52fe581e1fe71248b5e83f9
Anushka Biswas <anushkabiswas796@gmail.com>
2024-03-06 18:10:31 +0530

    feat: add homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import NavbarComponent from "./_components/Navbar/navbar";
+import styles from "./home.module.css";
 
 const Home = () => {
   return (
-    <main className="bg-white text-3xl text-center">
-      <NavbarComponent/>
-      
-
+    <main className={`${styles.mainHome} text-3xl text-center`}>
+      <NavbarComponent />
+      <div className={styles.contents}>
+        <div className ={styles.homeContainer}  >
+          <h1 className={styles.homeMainText}>Welcome.<br /></h1>
+          <h5 className={styles.homeSecondaryText}> To a satellite derived water retention solutions</h5>
+          <button className={styles.startedButton}>GET STARTED</button>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/projection2.png" height={420} width={420} alt="logo"/>
+        </div>
+      </div>
     </main>
   );
 };
