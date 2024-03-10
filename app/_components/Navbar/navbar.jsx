8accc09b43f595fa86dc76d5dc9946be6e87d217
SRIJEETA MUKHERJEE <srijeeta.mukherjee2021@gmail.com>
2024-03-10 22:00:17 +0530

    Aboutus page and updated the route in navbar

--- app/_components/Navbar/navbar.jsx
+++ app/_components/Navbar/navbar.jsx
@@ -16,7 +16,7 @@ const NavbarComponent = () => {
                 <div className="space-x-12">
                     <Link href="/" className={`${styles.navText} hover:text-emerald-700 text-xl font-bold`}>Home</Link>
                     <Link href="/" className={`${styles.navText} hover:text-emerald-700 text-xl font-bold`}>About</Link>
-                    <Link href="/" className={`${styles.navText} hover:text-emerald-700 text-xl font-bold`}>Contact</Link>
+                    <Link href="/user/aboutus" className={`${styles.navText} hover:text-emerald-700 text-xl font-bold`}>Contact</Link>
                     <Link href="/user/login" className={`${styles.navBtn} hover:text-emerald-700 text-xl text-teal-900 py-2 px-8 rounded-full font-bold`}>LOGIN</Link>
                 </div>
             </nav>
